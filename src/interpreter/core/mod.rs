44b35016866e717333186e92cd897e6717595d6c
santi34mg <ssilvera@correo.um.edu.uy>
2026-02-05 18:46:08 -0300

    Add interpreter core and builtins

--- src/interpreter/core/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod value;
-pub mod vm;
-pub mod eval;
-
-pub use value::Value;
-pub use vm::Interpreter;
--- src/interpreter/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod core;
-pub mod builtins;
-
-pub use core::{Interpreter, Value};
